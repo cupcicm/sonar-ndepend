2691dcd7a4b299524e6ece2cb72fac866b011799
Manu Cupcic <m.cupcic@criteo.com>
2014-11-17 14:40:33 +0100

    [Rules] Adds a RulesDefinition.
    And a very basic 'rules' file. This rules file will serve two
    purposes : it is used to define the RulesRepository (static
    list of rules made available by this plugin) and it will be used
    to generate a .ndproj from the .ndproj template.
    Also, changes the sonar API from version 3.7 to version 4.5
    
    Change-Id: I4ec8950496f15d749cb081e06e7f4e69a25cb673

---
+++ src/test/java/org/sonar/plugins/ndepend/NdependRulesDefinitionTest.java
@@ -0,0 +1,47 @@
+/*
+ * SonarQube NDepend Plugin
+ * Copyright (C) 2014 Criteo
+ * dev@sonar.codehaus.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sonar.plugins.ndepend;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.api.server.rule.RulesDefinition.Repository;
+import org.sonar.api.server.rule.RulesDefinitionXmlLoader;
+
+public class NdependRulesDefinitionTest {
+
+  @Test
+  public void test() {
+    RulesDefinition.Context context = new RulesDefinition.Context();
+    NdependRulesDefinition definition = new NdependRulesDefinition(new RulesDefinitionXmlLoader());
+
+    definition.define(context);
+    Repository repo = context.repository(NDependConfig.REPOSITORY_KEY);
+
+    List<RulesDefinition.Rule> rules = repo.rules();
+    assertThat(rules.size()).isEqualTo(2);
+    for (RulesDefinition.Rule rule : rules) {
+      assertThat(rule.key()).isNotNull();
+      assertThat(rule.name()).isNotNull();
+      assertThat(rule.htmlDescription()).isNotNull();
+    }
+  }
+}
