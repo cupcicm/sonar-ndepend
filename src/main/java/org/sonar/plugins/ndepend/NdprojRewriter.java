15df65308417d3bdd593e78246602ba7ba501575
Manu Cupcic <m.cupcic@criteo.com>
2014-11-18 16:01:47 +0100

    [ndproj] Adds a class that rewrites .ndproj files
    So that we can replace the <rules> element with rules generated
    from the rules.xml file.
    For the moment, this class doesn't do the real replacement, I'll
    add this in subsequent reviews.
    
    Change-Id: I6d23c3c1a01db955e5f34156942b490e4a25f9e4

---
+++ src/main/java/org/sonar/plugins/ndepend/NdprojRewriter.java
@@ -0,0 +1,106 @@
+/*
+ * SonarQube NDepend Plugin
+ * Copyright (C) 2014 Criteo
+ * dev@sonar.codehaus.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sonar.plugins.ndepend;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * Takes a .ndproj XML file, and replaces the &lt;rules&gt; element
+ * with the rules provided.
+ *
+ * Note : It will delete all rules and replace them, not merely add
+ * the new rules at the end.
+ */
+public class NdprojRewriter {
+
+  private final Document ndproj;
+
+  public NdprojRewriter(Document ndproj) {
+    this.ndproj = ndproj;
+    NodeList rulesElements = getRulesElement();
+    if (rulesElements.getLength() == 0) {
+      throw new IllegalArgumentException("Your document has no <rules/> element");
+    }
+    if (rulesElements.getLength() > 1) {
+      throw new IllegalArgumentException("Your document has strictly more than 1 <rules/> element");
+    }
+  }
+
+  /**
+   * Creates a rewriter from a .ndproj file.
+   */
+  public static NdprojRewriter fromFile(File file) throws SAXException, IOException, ParserConfigurationException {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    return new NdprojRewriter(factory.newDocumentBuilder().parse(file));
+  }
+
+  private NodeList getRulesElement() {
+    return ndproj.getElementsByTagName("rules");
+  }
+
+  /**
+   * Writes the .ndproj with the replaced rules to a writer object
+   * (for example a file)
+   */
+  public void writeTo(Writer writer) {
+    // The instantiation checks guarantees there is exactly one <rules> element.
+    Node rules = getRulesElement().item(0);
+    rules.getParentNode().replaceChild(getRules(), rules);
+    write(writer);
+  }
+
+  private void write(Writer writer) {
+    Transformer transformer;
+    try {
+      transformer = TransformerFactory.newInstance().newTransformer();
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+      DOMSource source = new DOMSource(ndproj);
+      StreamResult result = new StreamResult(writer);
+      transformer.transform(source, result);
+    } catch (TransformerConfigurationException e) {
+      throw new RuntimeException(e);
+    } catch (TransformerFactoryConfigurationError e) {
+      throw new RuntimeException(e);
+    } catch (TransformerException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Node getRules() {
+    // TODO(m.cupcic): Implement this
+    return ndproj.createElement("rules");
+  }
+}
---
+++ src/test/java/org/sonar/plugins/ndepend/NdprojRewriterTest.java
@@ -0,0 +1,81 @@
+/*
+ * SonarQube NDepend Plugin
+ * Copyright (C) 2014 Criteo
+ * dev@sonar.codehaus.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sonar.plugins.ndepend;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.StringWriter;
+import java.io.Writer;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import com.google.common.base.Joiner;
+
+public class NdprojRewriterTest {
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void TestCanReplaceRulesElement() throws Exception {
+    Document doc = prepareDocument(1);
+    Writer writer = new StringWriter();
+    new NdprojRewriter(doc).writeTo(writer);
+    String xml = Joiner.on(System.getProperty("line.separator"))
+                       .join(new String[] {"<root>", "<rules/>", "</root>", "" });
+    assertThat(writer.toString()).endsWith(xml);
+  }
+
+  @Test
+  public void TestCantInstantiateIfNoRulesElement() throws Exception {
+    Document doc = prepareDocument(0);
+    Writer writer = new StringWriter();
+    thrown.expect(IllegalArgumentException.class);
+    new NdprojRewriter(doc).writeTo(writer);
+  }
+
+  @Test
+  public void TestCantInstantiateIfTooMany()throws Exception {
+    Document doc = prepareDocument(2);
+    Writer writer = new StringWriter();
+    thrown.expect(IllegalArgumentException.class);
+    new NdprojRewriter(doc).writeTo(writer);
+  }
+
+  private Document prepareDocument(int howManyRulesElement) throws ParserConfigurationException {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    Document doc = factory.newDocumentBuilder().newDocument();
+
+    Node root = doc.createElement("root");
+    for (int i = 0; i < howManyRulesElement; i++) {
+      Node rules = doc.createElement("rules");
+      root.appendChild(rules);
+      rules.appendChild(doc.createElement("rule1"));
+    }
+
+    doc.appendChild(root);
+    return doc;
+  }
+}
